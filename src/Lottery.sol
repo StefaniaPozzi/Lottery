6c48ba6237463c1b0f8562b0e110fceca27c42fd
StefaniaPozzi <stefania.pozzi.it@gmail.com>
2023-08-31 08:06:08 +0200

    buy ticket event

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,5 +1,4 @@
 //SPDX-License-Indentifier:MIT
-
 pragma solidity ^0.8.18;
 
 /**
@@ -9,22 +8,26 @@ pragma solidity ^0.8.18;
  * @dev implements chainlink VRF V2 and chainlink automation
  */
 contract Lottery {
-    //errors
+    /** Layout of contracts: ErTS, EvMF
+     * ErTS
+     * errors, types, state variables
+     * EvMF
+     * events, modifiers, fucntions
+     */
     error Lottery__notEnoughEth__error();
-    //types
-
-    //state variables
-    uint256 private i_ticketPrice;
+    // @dev duration of the lottery
+    uint256 private immutable i_lotteryDuration;
+    uint256 private immutable i_ticketPrice;
     address payable[] private s_players;
+    // @dev always a smaller number than block.timestamp
+    uint256 private s_lastTimestampSnapshot;
 
-    //events
+    event Lottery__playerAccepted__event(address indexed player);
 
-    //modifiers
-
-    //functions
-
-    constructor(uint256 _ticketPrice) {
+    constructor(uint256 _ticketPrice, uint256 _lotteryDuration) {
         i_ticketPrice = _ticketPrice;
+        i_lotteryDuration = _lotteryDuration;
+        s_lastTimestampSnapshot = block.timestamp;
     }
 
     function buyTicket() external payable {
@@ -32,13 +35,21 @@ contract Lottery {
             revert Lottery__notEnoughEth__error();
         }
         s_players.push(payable(msg.sender));
+        emit Lottery__playerAccepted__event(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimestampSnapshot) < i_lotteryDuration) {
+            revert();
+        }
+    }
 
-    /*Getters*/
-
+    // Getters
     function getTicketPrice() public returns (uint256) {
         return i_ticketPrice;
+    }
+
+    function getLotteryDuration() public returns (uint256) {
+        return i_lotteryDuration;
     }
 }
